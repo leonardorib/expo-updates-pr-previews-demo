94a02491f7e799b0bb8fdce33e6315864326a8eb
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-12-16 21:43:28 -0300

    Configured demo

--- App.tsx
+++ App.tsx
@@ -1,95 +1,171 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+
 import {
+  Alert,
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import * as Updates from 'expo-updates';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const {currentlyRunning} = Updates.useUpdates();
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView style={styles.backgroundStyle}>
+      <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        style={styles.backgroundStyle}
+        contentContainerStyle={styles.scrollContentContainer}>
+        <View style={styles.section}>
+          <Text style={styles.titleText}>Expo Updates Constants</Text>
+          <Text style={styles.regularText}>
+            Updates.isEnabled: {`${Updates.isEnabled}`}
+          </Text>
+          <Text style={styles.regularText}>
+            Updates.channel: {`${Updates.channel}`}
+          </Text>
+          <Text style={styles.regularText}>
+            Updates.updateId: {`${Updates.updateId}`}
+          </Text>
+          <Text style={styles.regularText}>
+            Updates.checkAutomatically: {`${Updates.checkAutomatically}`}
+          </Text>
+          <Text style={styles.regularText}>
+            Updates.createdAt: {`${Updates.createdAt}`}
+          </Text>
+          <Text style={styles.regularText}>
+            Updates.isEmbeddedLaunch: {`${Updates.isEmbeddedLaunch}`}
+          </Text>
+        </View>
+
+        <View style={styles.spacer} />
+
+        <View style={styles.section}>
+          <Text style={styles.titleText}>Expo Updates Methods</Text>
+
+          <Button
+            title="currentlyRunning"
+            onPress={() => {
+              Alert.alert(
+                'currentlyRunning',
+                `${
+                  currentlyRunning
+                    ? JSON.stringify(currentlyRunning, null, 2)
+                    : 'N/A'
+                }`,
+              );
+            }}
+          />
+
+          <Button
+            title="checkForUpdateAsync"
+            onPress={async () => {
+              try {
+                const update = await Updates.checkForUpdateAsync();
+                Alert.alert(
+                  'checkForUpdateAsync',
+                  JSON.stringify(update, null, 2),
+                );
+              } catch (error: any) {
+                Alert.alert(
+                  'checkForUpdateAsync ERROR',
+                  error.message ?? error,
+                );
+              }
+            }}
+          />
+
+          <Button
+            title="fetchUpdateAsync"
+            onPress={async () => {
+              try {
+                const update = await Updates.fetchUpdateAsync();
+                Alert.alert(
+                  'fetchUpdateAsync',
+                  JSON.stringify(update, null, 2),
+                );
+              } catch (error: any) {
+                Alert.alert('fetchUpdateAsync ERROR', error.message ?? error);
+              }
+            }}
+          />
+
+          <Button
+            title="reloadAsync"
+            onPress={async () => {
+              try {
+                await Updates.reloadAsync();
+              } catch (error: any) {
+                Alert.alert('reloadAsync ERROR', error.message ?? error);
+              }
+            }}
+          />
+
+          <Button
+            title="clearLogEntriesAsync"
+            onPress={async () => {
+              try {
+                await Updates.clearLogEntriesAsync();
+
+                Alert.alert('clearLogEntriesAsync', 'Logs cleared');
+              } catch (error: any) {
+                Alert.alert(
+                  'clearLogEntriesAsync ERROR',
+                  error.message ?? error,
+                );
+              }
+            }}
+          />
+
+          <Button
+            title="readLogEntriesAsync"
+            onPress={async () => {
+              try {
+                const logs = await Updates.readLogEntriesAsync();
+                Alert.alert(
+                  'readLogEntriesAsync',
+                  logs.map(log => JSON.stringify(log, null, 2)).join('\n\n'),
+                );
+              } catch (error: any) {
+                Alert.alert(
+                  'readLogEntriesAsync ERROR',
+                  error.message ?? error,
+                );
+              }
+            }}
+          />
+
+          <Button
+            title="getExtraParamsAsync"
+            onPress={async () => {
+              try {
+                const extra = await Updates.getExtraParamsAsync();
+                Alert.alert(
+                  'getExtraParamsAsync',
+                  extra ? JSON.stringify(extra, null, 2) : 'N/A',
+                );
+              } catch (error: any) {
+                Alert.alert(
+                  'getExtraParamsAsync ERROR',
+                  error.message ?? error,
+                );
+              }
+            }}
+          />
+        </View>
+
+        <View style={styles.spacer} />
+
+        <View style={styles.section}>
+          <Text style={styles.regularText}>
+            __DEV__: {__DEV__ ? 'true' : 'false'}
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -97,21 +173,31 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  backgroundStyle: {
+    backgroundColor: 'white',
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  regularText: {
+    fontSize: 16,
+    color: 'black',
   },
-  highlight: {
-    fontWeight: '700',
+  scrollContentContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  spacer: {
+    height: 40,
+  },
+  section: {
+    gap: 12,
+    alignItems: 'center',
   },
 });
 
